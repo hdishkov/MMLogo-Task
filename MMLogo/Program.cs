99e79a1fd66ba40d55be957e0ea7fb14846ca457
thespooner <thespooner@students.softuni.bg>
2020-02-21 13:57:57 +0200

    Removed comments and stopwatch

--- MMLogo/Program.cs
+++ MMLogo/Program.cs
@@ -7,63 +7,9 @@ namespace MMLogo
     {
         static void Main(string[] args)
         {
-            Stopwatch stopwatch = new Stopwatch();
-            stopwatch.Start();
             int n = int.Parse(Console.ReadLine());
 
-            stopwatch.Stop();
-            Console.WriteLine(stopwatch.ElapsedMilliseconds);
             Console.WriteLine(PrintLogo.Print(n));
-            
-            //for (int i = 0; i <= n / 2; i++)
-            //{
-            //    //Upper half
-            //    Console.Write(new string('-', n - i));
-            //    Console.Write(new string('*', n + i));
-            //    Console.Write(new string('*', i));
-            //    Console.Write(new string('-', n - i - i));
-            //    Console.Write(new string('*', n + i));
-            //    Console.Write(new string('*', i));
-            //    Console.Write(new string('-', n - i)); 
-                
-            //    Console.Write(new string('-', n - i));
-            //    Console.Write(new string('*', n + i));
-            //    Console.Write(new string('*', i));
-            //    Console.Write(new string('-', n - i - i));
-            //    Console.Write(new string('*', n + i));
-            //    Console.Write(new string('*', i));
-            //    Console.Write(new string('-', n - i));
-
-            //    //Lower half
-
-            //    Console.WriteLine();
-            //}
-
-            //for (int j = n / 2; j >= 0; j--)
-            //{
-            //    Console.Write(new string('-', j));
-            //    Console.Write(new string('*', n));
-            //    Console.Write(new string('-', n - j - j));
-            //    Console.Write(new string('*', n + j));
-            //    Console.Write(new string('*', j));
-            //    Console.Write(new string('-', n - j - j));
-            //    Console.Write(new string('*', n));
-            //    Console.Write(new string('-', j)); 
-                
-            //    Console.Write(new string('-', j));
-            //    Console.Write(new string('*', n));
-            //    Console.Write(new string('-', n - j - j));
-            //    Console.Write(new string('*', n + j));
-            //    Console.Write(new string('*', j));
-            //    Console.Write(new string('-', n - j - j));
-            //    Console.Write(new string('*', n));
-            //    Console.Write(new string('-', j));
-
-
-            //    Console.WriteLine();
-            //}
-
-
         }
     }
 }
